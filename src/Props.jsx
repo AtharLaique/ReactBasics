a1805114a887041f293d7d32d23638d88768a0e3
Athar Laique <atharlaique15@gmail.com>
2020-02-29 17:42:34 +0500

    Write a class component for props

--- src/Props.jsx
+++ src/Props.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React , {Component }from 'react';
 //Step 2 : Receive Props here 
-const Props=(props)=>{
-    return(
+// const Props=(props)=>{
+//     return(
+//         <div>
+//         {/* Step 3 : Use Props here */}
+//         <h1> My Name is {props.name}</h1>
+//          {/* Step 3 : Get Html using props.children property */}
+//         {props.children}
+//         </div>
+//     ) 
+// }
+class Props extends Component{
+    render(){
+        return(
         <div>
-        {/* Step 3 : Use Props here */}
-        <h1> My Name is {props.name}</h1>
-         {/* Step 3 : Get Html using props.children property */}
-        {props.children}
-        </div>
-    ) 
+            <h1>My Name is </h1></div>
+        )
+    }
 }
 export default Props;
