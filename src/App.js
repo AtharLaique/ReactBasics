c2b6f222998572d23fabd91f4d05fddbb958fc14
Athar Laique <atharlaique15@gmail.com>
2020-03-01 16:03:31 +0500

    Update state in functional component

--- src/App.js
+++ src/App.js
@@ -5,14 +5,21 @@ import Props from './Props';
 
 function App() {
   //Declare the state
-  const[nameOfstate , methodToSetstate]=useState({name:'Athar from functional state'})
+  const[nameOfstate , methodToSetstate]=useState({name:'Athar from functional state'});
+  const update=()=>{
+    //Update state here
+   methodToSetstate({name:' Athar Laique'})
+}
   return (
     <div className="App">
       {/* Step 1 : Pass Data to component */}
       {/* use of state */}
       <Props name={nameOfstate.name} />
+      <button onClick={update}> update state</button>
     </div>
   );
 }
 
 export default App;
+
+
