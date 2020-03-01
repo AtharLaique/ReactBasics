b78e2406f81501c94abe795901db4807ec91a437
Athar Laique <atharlaique15@gmail.com>
2020-03-01 15:53:19 +0500

    Make state in functional component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+//Step 1 : import useState
+import React ,{useState}from 'react';
 import './App.css';
 import Props from './Props';
 
 function App() {
+  //Declare the state
+  const[nameOfstate , methodToSetstate]=useState({name:'Athar'})
   return (
     <div className="App">
       {/* Step 1 : Pass Data to component */}
-      <Props type='text' placeholder=" Enter Your name"/>
+      <Props />
     </div>
   );
 }
